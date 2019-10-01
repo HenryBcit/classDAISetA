53ee4d85ff7e22f23b9bd4d9a5a2d196d646d5e1
Necloed <abhimannyucc@gmail.com>
2019-10-01 11:45:02 -0700

    Made New Comp
    Displays Name

---
+++ styles/rancom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default function Name(){
+    return(
+        <View>
+            <Text style={{textAlign:"center", fontSize:60, color:"black"}}>Hello</Text>
+        </View>
+    )
+}
