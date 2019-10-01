21fe910a99f59d750153c957261c81aa0bfbd530
HenryBCIT <hleung70@bcit.ca>
2019-10-01 11:20:17 -0700

    final commit

---
+++ comps/Main.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Button
+} from 'react-native';
+//import styles from './styles/MainStyles';
+
+function Main(){
+  const [showMenu, setShowMenu] = useState(false);
+
+  var menu = null;
+  if(showMenu === true){
+    menu = (
+      <View>
+        <Button
+          title="Page 1"
+          onPress={()=>{}}
+        />
+        <Button
+          title="Page 2"
+          onPress={()=>{}}
+        />
+      </View>
+    )
+  }
+
+  return (
+    <View>
+      {menu}
+      <Button
+        title="Menu"
+        onPress={()=>{
+          setShowMenu(!showMenu);
+        }}
+      />
+      <Button
+        title="Image"
+        onPress={()=>{
+        }}
+      />
+    </View>
+  );
+};
+
+export default Main;
